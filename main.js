f0ea34a9c797a01f0ded90e48c2535ad522d2762
Azamatjon <azamatergasev01@gmail.com>
2024-04-03 15:42:20 +0500

    1 task done

---
+++ main.js
@@ -0,0 +1,23 @@
+// task 1 starts  
+
+// let arr = [1,3,5,6,7] 
+// let evenNumber = 0
+// let oddNumber = 0
+
+
+// for (let i = 0; i < arr.length; i++) {
+//     if(arr[i] % 2 == 0) {
+//         evenNumber ++
+//     }else{
+//         oddNumber++
+//     }
+// }
+// console.log(`juft sonlar yigindisi ${evenNumber} ga teng`);
+// console.log(`toq sonlar yigindisi ${oddNumber} ga teng`);
+
+//  1 task finished 
+
+
+//  2 task starts 
+
+
